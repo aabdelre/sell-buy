34d890e7ffb2b5921855d932d0aec34fa217ca6e
aabdelre <aabdelre@hamilton.edu>
2023-09-15 23:45:56 +0300

    NavBar

--- app/components/nav/navBar.tsx
+++ app/components/nav/navBar.tsx
@@ -1,5 +1,25 @@
+import Container from "../container";
+import Link from "next/link";
+import { Redressed } from "next/font/google";
+
+const redressed = Redressed({subsets: ['latin'], weight: ["400"]})
+
 const NavBar = () => {
-    return ( <div>NavBar</div> );
+    return ( <div className="sticky top-0 w-full bg-[#032B76] z-30 shadow-sm text-white">
+        <div className="py-4 border-w-[1px]">
+            <Container>
+                <div className="flex items-center justify-between gap-3 md:gap-0">
+                    <Link href="/" className={`${redressed.className} font-bold text-2xl`}>College Market</Link>
+                    <div className="hidden md:block">SearchBox</div>
+                    <div className="flex items-center gap-8 md:gap-12">
+                        <div>Cart</div>
+                        <div>UserMenu</div>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    </div> 
+    );
 }
  
 export default NavBar;
